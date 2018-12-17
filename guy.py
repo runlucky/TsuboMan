95fa224924a59c7e0c479cb3ab700472df0dec6b
Kakeru Fukuda <stand.by.we@gmail.com>
2018-12-17 17:00:13 +0900

    test2

--- guy.py
+++ guy.py
@@ -24,3 +24,21 @@ class Guy:
         pyxel.text(0, 0, "abcedfg", 2)
         pyxel.blt(0, self.y, 0, 0, 0, 16, 16, 0)
 
+
+
+class ClassOne:
+    def method1(self):
+        classtwoObj.method2()
+
+class ClassTwo:
+    def method2(self):
+        curframe = inspect.currentframe()
+        calframe = inspect.getouterframes(curframe, 4)
+        print ('\nI was called from', calframe[1][3], 'in', calframe[1][4][0][6: -2])
+
+# create objects to access class methods
+classoneObj = ClassOne()
+classtwoObj = ClassTwo()
+
+# start the program
+classoneObj.method1()
